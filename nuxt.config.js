44de157542014b1dec858f81b8e87c6525bbfc31
dev0715 <lavelle.warren.1995@gmail.com>
2021-11-07 18:00:14 +0200

    initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,97 @@
+import colors from 'vuetify/es5/util/colors'
+
+export default {
+  // Target (https://go.nuxtjs.dev/config-target)
+  target: 'static',
+
+  // Global page headers (https://go.nuxtjs.dev/config-head)
+  head: {
+    titleTemplate: '%s - IOT WEB LAB, LLC',
+    title: 'Home',
+    htmlAttrs: {
+      lang: 'en',
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content:
+          'IOT WEB LAB, LLC - Hire world class offshore remote experts for all kinds of digital design, development and consultancy. We transform your idea into a viable digital product, using our decade-long experience and the latest technology in digital product development.',
+      },
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon/favicon.ico' },
+      { rel: 'manifest', href: '/site.webmanifest' },
+    ],
+  },
+
+  // Global CSS (https://go.nuxtjs.dev/config-css)
+  css: [],
+
+  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
+  plugins: [],
+
+  // Auto import components (https://go.nuxtjs.dev/config-components)
+  // components: true,
+  components: [
+    '~/components', // shortcut to { path: '~/components' }
+    { path: '~/components/sections/', prefix: 'sections' },
+  ],
+
+  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
+  buildModules: [
+    // https://go.nuxtjs.dev/eslint
+    '@nuxtjs/eslint-module',
+    // https://go.nuxtjs.dev/vuetify
+    '@nuxtjs/vuetify',
+  ],
+
+  // Modules (https://go.nuxtjs.dev/config-modules)
+  modules: [],
+
+  // Vuetify module configuration (https://go.nuxtjs.dev/config-vuetify)
+  vuetify: {
+    customVariables: ['~/assets/variables.scss'],
+    theme: {
+      dark: false,
+      themes: {
+        dark: {
+          primary: colors.blue.darken2,
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3,
+        },
+        light: {
+          primary: colors.blue.darken2,
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3,
+        },
+      },
+    },
+  },
+
+  // Build Configuration (https://go.nuxtjs.dev/config-build)
+
+  build: {
+    extend(config, ctx) {
+      config.module.rules.push({
+        enforce: 'pre',
+        test: /\.(js|vue)$/,
+        loader: 'eslint-loader',
+        exclude: /(node_modules)/,
+        options: {
+          fix: true,
+        },
+      })
+    },
+  },
+}
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <section id="home">
+    <v-row no-gutters>
+      <v-col cols="12">
+        <SectionsHero />
+        <SectionsIntro />
+        <SectionsCalloutBlock />
+        <SectionsFeatures />
+        <SectionsBrands />
+        <SectionsTestimonials />
+      </v-col>
+    </v-row>
+  </section>
+</template>
