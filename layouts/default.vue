5ab1d15a3027cb6ba8c4751629d820ec95bd27d8
dev0715 <toptal126@outlook.com>
2021-11-07 18:00:14 +0200

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-app dark>
+    <siteHeader />
+    <v-main>
+      <nuxt />
+      <footerTop />
+    </v-main>
+    <siteFooter />
+    <Notification />
+  </v-app>
+</template>
+
+<script>
+import siteHeader from '~/components/siteHeader.vue'
+import footerTop from '~/components/footerTop.vue'
+import siteFooter from '~/components/siteFooter.vue'
+export default {
+  components: {
+    siteHeader,
+    footerTop,
+    siteFooter,
+  },
+  head() {
+    return {
+      script: [
+        {
+          src: 'https://www.googletagmanager.com/gtag/js?id=G-0DYDMZEYDZ',
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style></style>
