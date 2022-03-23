6d2c9f510530bc148f1b8cc545a1c43ba5c65ffc
dev0715 <lavelle.warren.1995@gmail.com>
2022-03-23 18:45:37 +0200

    add websites page in services

---
+++ pages/services/websites.vue
@@ -0,0 +1,23 @@
+<template>
+  <section id="mobile">
+    <SectionsHeroAlt :hero-alt="heroAlt" />
+    <SectionsWebsitesTypeList />
+  </section>
+</template>
+<script>
+export default {
+  //   async fetch({ store }) {
+  //     await store.dispatch('ecommerce/fetchEcommerceScopes')
+  //   },
+  data() {
+    return {
+      heroAlt: [
+        {
+          src: 'pexels-ekaterina-bolovtsova-4048767.jpg',
+          heading: 'Websites',
+        },
+      ],
+    }
+  },
+}
+</script>
