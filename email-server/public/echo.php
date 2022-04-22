222906f84501db8d191889cc4ab0e5391148132d
dev0715 <lavelle.warren.1995@gmail.com>
2022-04-22 11:01:01 +0300

    check ip & region: email sender

--- email-server/public/echo.php
+++ email-server/public/echo.php
@@ -1,6 +1,20 @@
 <?php
-echo $_ENV ['USER_EMAIL'];
-echo $_ENV ['USER_PASSWORD'];
-echo $_ENV ['SMTP_SERVER'];
-echo $_ENV ['SMTP_PORT'];
-echo $_ENV ['MAIL_ENCRIPTYION'];
+if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+    $ip = $_SERVER['HTTP_CLIENT_IP'];
+} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+} else {
+    $ip = $_SERVER['REMOTE_ADDR'];
+}
+
+
+$location = file_get_contents("http://ipinfo.io/".$ip."/geo");
+$location = json_decode($location, true);
+$country  = $location['country'];
+$region   = $location['region'];
+$city     = $location['city'];
+
+echo '<div>'.$contry."</div>";
+echo '<div>'.$region."</div>";
+echo '<div>'.$city."</div>";
+echo '<div>'.$ip."</div>";
--- email-server/public/send_mail.php
+++ email-server/public/send_mail.php
@@ -50,18 +50,64 @@ function getResponse($code, $message)
     ));
 }
 function sendSelfMail($request){
+    if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+        $ip = $_SERVER['HTTP_CLIENT_IP'];
+    } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    } else {
+        $ip = $_SERVER['REMOTE_ADDR'];
+    }  
+    
+    $location = file_get_contents("http://ipinfo.io/".$ip."/geo");  
+    $location = json_decode($location, true);
+    $region   = $location['region'];
+    $city     = $location['city'];
+    $country  = $location['country'];
+
     $transport = (new Swift_SmtpTransport($_ENV ['SMTP_SERVER'], $_ENV ['SMTP_PORT']))
       ->setUsername($_ENV ['USER_EMAIL'])
       ->setPassword($_ENV ['USER_PASSWORD'])
       ->setEncryption($_ENV ['MAIL_ENCRIPTYION']);
     $mailer = new Swift_Mailer($transport);
+
     $message = (new Swift_Message('Question from IOT WEB LAB site'))
       ->setFrom([$request["email"] => $request["name"]])
       ->setTo([$_ENV ['USER_EMAIL'] => 'IOT WEB LAB'])
-      ->setBody('User name: '.$request["name"].'
-          Email: '.$request["email"].'
-          Link: '.$request["link"].'
-          Message: '.$request["message"]);
+      ->addPart('
+        <div style="margin-top: 10px; font-size: 14px">
+          <div style="width: 100px; display: inline-block"><b>User name: </b></div><div style=" display: inline-block">'.$request["name"].'</div>
+        </div>
+        <div style="margin-top: 10px; font-size: 14px">
+          <div style="width: 100px; display: inline-block"><b>Email: </b></div><div style=" display: inline-block">'.$request["email"].'</div>
+        </div>
+        <div style="margin-top: 10px; font-size: 14px">  
+          <div style="width: 100px; display: inline-block"><b>Link: </b></div><div style=" display: inline-block">'.$request["link"].'</div>
+        </div>
+        <hr style="margin-top: 10px;">
+        <div style="margin-top: 50px; font-size: 14px">
+          <h1 style="text-align: center;">'.$request["subject"].'</h1>
+          <p style="padding-left: 20px;">'.$request["message"].'</p>
+        </div>
+        <hr style="margin-top: 10px;">
+        <table style="margin-top: 50px">
+          <thead>
+              <tr>
+                  <th cols="3" style="width: 100px">IP</th>
+                  <th cols="3" style="width: 100px">Country</th>
+                  <th cols="3" style="width: 100px">Region</th>
+                  <th cols="3" style="width: 100px">City</th>
+              </tr>
+          </thead>
+          <tbody>
+              <tr>
+                  <td cols="3" style="width: 100px; text-align: center;">'.$ip.'</td>
+                  <td cols="3" style="width: 100px; text-align: center;">'.$country.'</td>
+                  <td cols="3" style="width: 100px; text-align: center;">'.$region.'</td>
+                  <td cols="3" style="width: 100px; text-align: center;">'.$city.'</td>      
+              </tr>
+          </tbody>
+        </table>', "text/html"
+        );
     if($request["file"] !== 'null'){
       $attachment = Swift_Attachment::fromPath($_FILES["file"]["tmp_name"])->setFilename($_FILES["file"]["name"]);
       $message->attach($attachment);
@@ -77,11 +123,18 @@ function sendMail($request){
       ->setPassword($_ENV ['USER_PASSWORD'])
       ->setEncryption($_ENV ['MAIL_ENCRIPTYION']);
     $mailer = new Swift_Mailer($transport);
-    $message = (new Swift_Message('Thanks for you question'))
+    $message = (new Swift_Message('Thanks for you question'));
+    $message
       ->setFrom([$_ENV ['USER_EMAIL'] => $request["IOT WEB LAB"]])
       ->setTo([$request["email"] => $request["name"]])
-      ->setBody('Dear '.$request["name"].'
-      '.$request["feedback"]);     
+      ->setBody(
+        '<html>' .
+        ' <body>' .
+        '<div style="padding: 20px">'.
+        '<img src="'.$message->embed(Swift_Image::fromPath('logo.png')).'" alt="logo" />'.
+      '</div>'.
+      '<h3>Dear, '.$request["name"].'</h3>'.
+      '<div>'.$request["feedback"].'</div>', "text/html");     
       
     $result = $mailer->send($message);
     return $result;
